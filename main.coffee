d430e1f7774ac1a90c8e8828615d0f4e7b56a998
Yu Liang <yu.liang@thekono.com>
2015-10-10 18:06:45 +0800

    Implement transition

--- main.coffee
+++ main.coffee
@@ -1,2 +1,38 @@
-screenWidth = document.documentElement.clientWidth
-console.log screenWidth
+SCREEN_WIDTH = document.documentElement.clientWidth
+
+buildSlides = ->
+
+    $('.slide').each (ix, slide) ->
+
+        $(slide)
+            .addClass('slide')
+            .addClass('is-absolute')
+            .css(top: 0, left: "#{ix * SCREEN_WIDTH}px")
+            .css(width: "#{SCREEN_WIDTH}px")
+
+
+    slideCount = $('.slide').length
+    $('#slide-container')
+        .css(width: "#{slideCount * SCREEN_WIDTH}px")
+
+    $('#cloud-container')
+        .css(width: "#{slideCount * SCREEN_WIDTH}px")
+
+
+gotoSlide = (slideIndex)->
+
+    $activeSlide = $('.slide.active')
+    activeSlideNum = parseInt($activeSlide.attr('id').substr(5), 10)
+    $targetSlide = $("#slide#{slideIndex + 1}")
+    targetSlideNum = parseInt($targetSlide.attr('id').substr(5), 10)
+
+    $slideContainer = $('#slide-container')
+    offset = -1 * (targetSlideNum - activeSlideNum) * SCREEN_WIDTH
+    currPos = parseInt($slideContainer.css('left'), 10)
+    $slideContainer.transition(left: "#{currPos + offset}px", 500, 'ease')
+
+    $activeSlide.toggleClass('active')
+    $targetSlide.toggleClass('active')
+
+
+buildSlides()
--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-var screenWidth;
+var SCREEN_WIDTH, buildSlides, gotoSlide;
 
-screenWidth = document.documentElement.clientWidth;
+SCREEN_WIDTH = document.documentElement.clientWidth;
 
-console.log(screenWidth);
+buildSlides = function() {
+  var slideCount;
+  $('.slide').each(function(ix, slide) {
+    return $(slide).addClass('slide').addClass('is-absolute').css({
+      top: 0,
+      left: "" + (ix * SCREEN_WIDTH) + "px"
+    }).css({
+      width: "" + SCREEN_WIDTH + "px"
+    });
+  });
+  slideCount = $('.slide').length;
+  $('#slide-container').css({
+    width: "" + (slideCount * SCREEN_WIDTH) + "px"
+  });
+  return $('#cloud-container').css({
+    width: "" + (slideCount * SCREEN_WIDTH) + "px"
+  });
+};
+
+gotoSlide = function(slideIndex) {
+  var $activeSlide, $slideContainer, $targetSlide, activeSlideNum, currPos, offset, targetSlideNum;
+  $activeSlide = $('.slide.active');
+  activeSlideNum = parseInt($activeSlide.attr('id').substr(5), 10);
+  $targetSlide = $("#slide" + (slideIndex + 1));
+  targetSlideNum = parseInt($targetSlide.attr('id').substr(5), 10);
+  $slideContainer = $('#slide-container');
+  offset = -1 * (targetSlideNum - activeSlideNum) * SCREEN_WIDTH;
+  currPos = parseInt($slideContainer.css('left'), 10);
+  $slideContainer.transition({
+    left: "" + (currPos + offset) + "px"
+  }, 500, 'ease');
+  $activeSlide.toggleClass('active');
+  return $targetSlide.toggleClass('active');
+};
+
+buildSlides();
